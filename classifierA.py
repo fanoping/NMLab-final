ceb9d96e35eb48e29146ca26883206c57b071c43
fanoping <fanoping@gmail.com>
2018-06-13 17:29:36 +0800

    modify

--- classifierA.py
+++ classifierA.py
@@ -14,15 +14,23 @@ def main(args):
             Classifier: K nearest neighboring (K = 3), decision tree classifier
             Validation: 10-folded cross validation
     """
-    csv_file = pd.read_csv(args.input_csv)
+    train_csv_file = pd.read_csv(args.train_csv)
+    test_csv_file = pd.read_csv(args.test_csv)
     config = json.load(open(args.config))['Scenario-A']
 
-    attributes = [csv_file[attr] for attr, usage in config["attribute"].items() if usage]
-    label = csv_file["label"] if config["label"] else ValueError("No label specified!")
+    attributes = [train_csv_file[attr] for attr, usage in config["attribute"].items() if usage]
+    label = train_csv_file["label"] if config["label"] else ValueError("No label specified!")
 
     train_x = np.array(attributes).T
     train_label = [1 if item == 'TOR' else 0 for item in label]
     train_label = np.array(train_label).T
+
+    test_attributes = [test_csv_file[attr] for attr, usage in config["attribute"].items() if usage]
+    test_label = test_csv_file["label"] if config["label"] else ValueError("No label specified!")
+
+    test_x = np.array(test_attributes).T
+    test_label = [1 if item == 'TOR' else 0 for item in test_label]
+    test_label = np.array(test_label).T
 
     def k_fold_cross_validation(k_fold, train_x, label):
         split = np.array_split(train_x, k_fold)
@@ -38,15 +46,15 @@ def main(args):
 
     splitted_data = list(k_fold_cross_validation(args.k, train_x, train_label))
 
+    if args.arch.lower() == 'knn':
+        neigh = KNeighborsClassifier(n_neighbors=3)
+    elif args.arch.lower() == 'tree':
+        neigh = DecisionTreeClassifier()
+    else:
+        return NotImplementedError
+
     total = 0
     for idx, (train, valid, train_label, valid_label) in enumerate(splitted_data):
-        if args.arch.lower() == 'knn':
-            neigh = KNeighborsClassifier(n_neighbors=3)
-        elif args.arch.lower() == 'tree':
-            neigh = DecisionTreeClassifier()
-        else:
-            return NotImplementedError
-
         train = np.concatenate(train)
         train_label = np.concatenate(train_label)
         neigh.fit(train, train_label)
@@ -56,17 +64,20 @@ def main(args):
         print('\tAccuracy: {:.6f}'.format(score))
         # print('\tPrecision: {:.6f}'.format(precision_score(valid_label, neigh.predict(valid))))
         # print('\tRecall: {:.6f}'.format(recall_score(valid_label, neigh.predict(valid))))
-
         total += score
     print("Ave: {:.6f}".format(total / args.k))
+
+    print("Test data fitting accuracy: {:.6f}".format(neigh.score(test_x, test_label)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Classifier for Scenario A')
-    parser.add_argument('-k', default=10, type=int,
+    parser.add_argument('-k', default=5, type=int,
                         help='k folded cross validation')
-    parser.add_argument('--input-csv', default='CSV/Scenario-A/SelectedFeatures-10s-TOR-NonTOR.csv',
-                        help='input information from csv file')
+    parser.add_argument('--train-csv', default='CSV/Scenario-A/SelectedFeatures-10s-TOR-NonTOR.csv',
+                        help='input information from csv file for training')
+    parser.add_argument('--test-csv', default='realtime.pcap_Flow.csv',
+                        help='input information from csv file for testing')
     parser.add_argument('--config', default='config.json',
                         help='specify the selected feature')
     parser.add_argument('--arch', default='knn', type=str,
--- classifierB.py
+++ classifierB.py
@@ -15,19 +15,26 @@ def main(args):
             Classifier: K nearest neighboring (K = 3), decision tree classifier, random forest
             Validation: k-folded cross validation
     """
-    csv_file = pd.read_csv(args.input_csv)
+    csv_file = pd.read_csv(args.train_csv)
+    test_csv_file = pd.read_csv(args.test_csv)
     config = json.load(open(args.config))['Scenario-B']
 
     attributes = [csv_file[attr] for attr, usage in config["attribute"].items() if usage]
+    test_attributes = [test_csv_file[attr] for attr, usage in config["attribute"].items() if usage]
 
     # labels
     label = csv_file["label"] if config["label"] else ValueError("No label specified!")
+    test_label = test_csv_file["label"] if config["label"] else ValueError("No label specified!")
     labels = {'BROWSING': 0, 'AUDIO': 1, 'CHAT': 2, 'MAIL': 3, 'P2P': 4,
               'FILE-TRANSFER': 5, 'VOIP': 6, 'VIDEO': 7}
 
     train_x = np.array(attributes).T
     train_label = [labels[item] for item in label]
     train_label = np.array(train_label).T
+
+    test_x = np.array(test_attributes).T
+    test_label = [labels[item] for item in test_label]
+    test_label = np.array(test_label).T
 
     def k_fold_cross_validation(k_fold, train_x, label):
         split = np.array_split(train_x, k_fold)
@@ -43,17 +50,17 @@ def main(args):
 
     splitted_data = list(k_fold_cross_validation(args.k, train_x, train_label))
 
+    if args.arch.lower() == 'knn':
+        neigh = KNeighborsClassifier(n_neighbors=3)
+    elif args.arch.lower() == 'tree':
+        neigh = DecisionTreeClassifier()
+    elif args.arch.lower() == 'forest':
+        neigh = RandomForestClassifier(n_estimators=20, random_state=2)
+    else:
+        raise NotImplementedError(args.arch)
+
     total = 0
     for idx, (train, valid, train_label, valid_label) in enumerate(splitted_data):
-        if args.arch.lower() == 'knn':
-            neigh = KNeighborsClassifier(n_neighbors=3)
-        elif args.arch.lower() == 'tree':
-            neigh = DecisionTreeClassifier()
-        elif args.arch.lower() == 'forest':
-            neigh = RandomForestClassifier(n_estimators=20, random_state=2)
-        else:
-            raise NotImplementedError(args.arch)
-
         train = np.concatenate(train)
         train_label = np.concatenate(train_label)
         neigh.fit(train, train_label)
@@ -67,13 +74,17 @@ def main(args):
         total += score
     print("Ave:", total/args.k)
 
+    print("Test data fitting accuracy: {:.6f}".format(neigh.score(test_x, test_label)))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Classifier for Scenario B')
     parser.add_argument('-k', default=20, type=int,
                         help='k folded cross validation')
-    parser.add_argument('--input-csv', default='CSV/Scenario-B/TimeBasedFeatures-10s-Layer2.csv',
+    parser.add_argument('--train-csv', default='CSV/Scenario-B/TimeBasedFeatures-10s-Layer2.csv',
                         help='input information from csv file')
+    parser.add_argument('--test-csv', default='realtime.pcap_Flow.csv',
+                        help='input information from csv file for testing')
     parser.add_argument('--config', default='config.json',
                         help='specify the selected feature')
     parser.add_argument('--arch', default='knn', type=str,
